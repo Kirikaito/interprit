bd384607133c253169e48534d92a9d328643896f
Kirikaito <116945391+Kirikaito@users.noreply.github.com>
2023-03-30 14:13:24 +0500

    Update alg.js

--- alg.js
+++ alg.js
@@ -10,86 +10,69 @@ class HuffmanNode {
     }  
 }
 
-class HuffmanCoding {
-    getCode(input){
-        var freqMap = this.buildFrequencyMap(input); 
-        var nodeQueue = this.sortByFrequence(freqMap);
-        this.root = this.buildTree(nodeQueue);
-        var codeMap = this.createHuffmanCode(this.root);
-        return codeMap;
-    }
-
-    buildFrequencyMap(input) {
-        var map = new Map();
-        for (let i = 0; i < input.length; i++) {
-            let ch = input.charAt(i);
+function buildFrequencyMap(input) {
+    var map = new Map();
+    for (let i = 0; i < input.length; i++) {
+        let ch = input.charAt(i);
             if (!map.has(ch)) {
                 map.set(ch, 1);
-            } else {
-                let val = map.get(ch);
-                map.set(ch, ++val);
-            }
+        } else {
+            let val = map.get(ch);
+            map.set(ch, ++val);
         }
-        return map;
     }
+    map.set("bolt1",1);
+    map.set("bolt2",1);
+    return map;
+}
     
-    sortByFrequence(map){
-        var queue = [];
-        for (let entry of map.entries()){queue.push(new HuffmanNode(entry[0], entry[1], null, null));}
-        queue.sort((a,b) => a.frequency - b.frequency )
-        return queue;
-    }  
+function sortByFrequence(map){
+    var queue = [];
+    for (let entry of map.entries()){queue.push(new HuffmanNode(entry[0], entry[1], null, null));}
+    queue.sort((a,b) => a.frequency - b.frequency )
+    return queue;
+}  
     
-    buildTree(nodeQueue) {
-        while (nodeQueue.length > 1) {
-            let node1 = nodeQueue.shift();
-            let node2 = nodeQueue.shift();
-            let node = new HuffmanNode('', node1.frequency + node2.frequency, node1, node2);  
-            nodeQueue.push(node);
-        }
-        return nodeQueue.shift();
+function buildTree(nodeQueue){
+    while (nodeQueue.length > 1) {
+        let node1 = nodeQueue.shift();
+        let node2 = nodeQueue.shift();
+        let node = new HuffmanNode('', node1.frequency + node2.frequency, node1, node2);  
+        nodeQueue.push(node);
     }
+    return nodeQueue.shift();
+}
     
-    createHuffmanCode(node) {
-    	var map = new Map();
-    	this.createCodeRec(node, map, "");
-    	return map;
-    }
+function createHuffmanCode(node){
+	var map = new Map();
+	createCodeRec(node, map, "");
+	return map;
+}
     
-    createCodeRec(node, map,  s){
-    	if (node.left == null && node.right == null){
-    		map.set(node.ch, s);
-            return;
-        }    
-    	this.createCodeRec(node.left, map, s + '0');
-    	this.createCodeRec(node.right, map, s + '1' );
-    }
+function createCodeRec(node, map,  s){
+	if (node.left == null && node.right == null){
+		map.set(node.ch, s);
+        return;
+    }    
+	createCodeRec(node.left, map, s + '0');
+	createCodeRec(node.right, map, s + '1' );
+}
 
-    encode(codeMap, input) {
+function encode(codeMap, input) {
         var s = ""
         for (let i = 0; i < input.length; i++) {
             s += codeMap.get(input.charAt(i));
         }
+        s+=codeMap.get("bolt1");
+        s+=codeMap.get("bolt2");
+        while(s.length % 8 != 0){s+="0";}
         return s;
-    }
-    
-	decode(coded) {
-	    var s = ""
-	    var curr = this.root;
-	    for (let i = 0; i < coded.length; i++) {
-	        curr = coded.charAt(i) == '1' ? curr.right : curr.left;
-	        if (curr.left == null && curr.right == null) {
-	            s += curr.ch;
-	            curr = this.root;
-	        }
-	    }
-	    return s;
-	}
 }
 
-var huffman = new HuffmanCoding();
-var codeMap = huffman.getCode(input);
 
-if (process.argv[3] == '-e'){console.log(huffman.encode(codeMap, input));}
-else if(process.argv[3] == '-d'){console.log(huffman.decode(input));}
+var freqMap = buildFrequencyMap(input); 
+var nodeQueue = sortByFrequence(freqMap);
+root = buildTree(nodeQueue);
+var codeMap = createHuffmanCode(root);
+if (process.argv[3] == '-e'){console.log(encode(codeMap, input));}
 else if(process.argv[3] == '-m'){console.log(codeMap)}
